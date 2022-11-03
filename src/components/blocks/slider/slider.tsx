68a53b9ea35b1a5233c0e0ef2398bbc6a8e72332
jiwon9412 <80965224+jiwon9412@users.noreply.github.com>
2022-11-03 23:20:39 +0900

    add Slider, Slider 컴포넌트화(blocks)

---
+++ src/components/blocks/slider/slider.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+interface SlideProps {
+  color?: string;
+  currSlide?: number;
+}
+
+interface DotProps {
+  active?: number;
+}
+
+const Slider = (): JSX.Element => {
+  const [currSlide, setCurrSlide] = useState(0);
+  const slide = (index: number): void => {
+    setCurrSlide(index);
+  };
+  return (
+    <SlideBox>
+      <ItemBox>
+        <SlideItem color='red' currSlide={currSlide} />
+        <SlideItem color='blue' />
+        <SlideItem color='green' />
+      </ItemBox>
+      <SlideDot active={currSlide + 1}>
+        <span onClick={() => slide(0)}></span>
+        <span onClick={() => slide(1)}></span>
+        <span onClick={() => slide(2)}></span>
+      </SlideDot>
+    </SlideBox>
+  );
+};
+
+export default Slider;
+
+const SlideBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const ItemBox = styled.div`
+  display: flex;
+  width: 100%;
+  height: 40vh;
+  white-space: nowrap;
+  overflow: hidden;
+`;
+
+const SlideItem = styled.div<SlideProps>`
+  flex: none;
+  background-color: ${(props) => props.color};
+  width: 100%;
+  height: 100%;
+  margin-left: ${(props) => `-${props.currSlide}00%`};
+  transition: margin 1000ms;
+`;
+
+const SlideDot = styled.div<DotProps>`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-top: 1em;
+
+  span {
+    display: block;
+    margin: 0 1em;
+    cursor: pointer;
+    width: 1em;
+    height: 1em;
+    border-radius: 50%;
+    border: 1px solid #385461;
+  }
+  span:nth-child(${(props) => props.active}) {
+    background-color: #385461;
+  }
+`;
--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,45 +1,14 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import AuthService from "../../../service/auth_service";
-
-interface SlideProps {
-  color?: string;
-  preSlide?: number;
-  currSlide?: number;
-}
-
-interface DotProps {
-  active?: number;
-}
+import Slider from "../../blocks/slider/slider";
 
 const Home = ({ authService }: { authService?: AuthService }): JSX.Element => {
   const location = useLocation();
-  const [preSlide, setPreSlide] = useState(1);
-  const [currSlide, setCurrSlide] = useState(1);
-  const [isActive, setIsActive] = useState(false);
-
-  const slide = (index?: number): void => {
-    if (currSlide !== index) {
-      setPreSlide(currSlide);
-    }
-    index && setCurrSlide(index);
-  };
-
-  console.log(`preSlide : ${preSlide}`);
-  console.log(`currSlide : ${currSlide}`);
   return (
     <HomeBox>
-      <SlideBox>
-        <SlideItem color='red' preSlide={preSlide} currSlide={currSlide} />
-        <SlideItem color='blue' />
-        <SlideItem color='green' />
-      </SlideBox>
-      <SlideDot active={currSlide}>
-        <span onClick={() => slide(1)}></span>
-        <span onClick={() => slide(2)}></span>
-        <span onClick={() => slide(3)}></span>
-      </SlideDot>
+      <Slider />
     </HomeBox>
   );
 };
@@ -55,94 +24,4 @@ const HomeBox = styled.section`
   width: 100%;
   height: 100%;
   padding: 10px 15%;
-`;
-
-const SlideBox = styled.div`
-  background-color: #ccc;
-  width: 100%;
-  height: 40vh;
-  white-space: nowrap;
-  overflow: hidden;
-`;
-
-const SlideItem = styled.div<SlideProps>`
-  display: inline-block;
-  background-color: ${(props) => props.color};
-  width: 100%;
-  height: 100%;
-  margin-left: 0%;
-  animation-duration: 1s;
-  animation-name: ${(props) =>
-    props.preSlide !== props.currSlide
-      ? `slide${props.preSlide}${props.currSlide}`
-      : ""};
-  animation-fill-mode: forwards;
-  @keyframes slide12 {
-    from {
-      margin-left: 0%;
-    }
-    to {
-      margin-left: -100%;
-    }
-  }
-  @keyframes slide13 {
-    from {
-      margin-left: 0%;
-    }
-    to {
-      margin-left: -200%;
-    }
-  }
-  @keyframes slide21 {
-    from {
-      margin-left: -100%;
-    }
-    to {
-      margin-left: 0%;
-    }
-  }
-  @keyframes slide23 {
-    from {
-      margin-left: -100%;
-    }
-    to {
-      margin-left: -200%;
-    }
-  }
-  @keyframes slide31 {
-    from {
-      margin-left: -200%;
-    }
-    to {
-      margin-left: 0%;
-    }
-  }
-  @keyframes slide32 {
-    from {
-      margin-left: -200%;
-    }
-    to {
-      margin-left: -100%;
-    }
-  }
-`;
-
-const SlideDot = styled.div<DotProps>`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  margin-top: 1em;
-
-  span {
-    display: block;
-    margin: 0 1em;
-    cursor: pointer;
-    width: 1em;
-    height: 1em;
-    border-radius: 50%;
-    border: 1px solid #385461;
-  }
-  span:nth-child(${(props) => props.active}) {
-    background-color: #385461;
-  }
 `;
